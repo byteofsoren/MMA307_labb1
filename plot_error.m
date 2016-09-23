401207228f92c2d47177b5220aa3eaaeef323834
byteofsoren <r00tr4t@gmail.com>
2016-09-24 00:42:10 +0200

    adde a line in the plot for p and touched the text a bit

--- plot_error.m
+++ plot_error.m
@@ -1,6 +1,7 @@
 function plot_error(f)
     % Plots the error of dx 
 
+    f = @(x) sin(x);
     h = 1./10.^(1:5);
     x = 1;
     yE = cos(x);
@@ -11,20 +12,26 @@ function plot_error(f)
         log_err2(i) = log(abs(yE - y(i, 2)));
         log_h(i) = log(h(i));
     end
-    disp('p is')
     p1 = 1; p2=2;
     for px = 1:4
         delta_x(px) = log_h(px) - log_h(px+1);
         delta_y(px) = log_err2(px) - log_err2(px+1);
         p(px) = delta_y/delta_x;
     end
+    disp('p(1)='); disp(p(1));
+    m = log_err2(1) - p(1) .* log_h(1)
+    disp('p=');
     disp(transpose(p));
     disp('medium p');
     disp(sum(p)/4)
     disp('log_h');
     disp(transpose(log_h));
+    axis([-12 -2 -30 -5]);
     hold on;
     grid on;
-    plot(log_h, log_err1)
-    plot(log_h, log_err2)
+    plot(log_h, log_err1, 'LineWidth', 3)
+    plot(log_h, log_err2, 'LineWidth', 3)
+    plot(log_h, p(1)*(log_h)+m,'--', 'LineWidth' , 3)
+    legend('cos(x)','D(sin(x))','P value', 'Location','northwest');
+    title('Error of derivitive');
 end
